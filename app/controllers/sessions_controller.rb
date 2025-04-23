af0b9fee79fbc1a5b3798eaf7e48e358b2b30bc9
alexander-rodionov <alexander.a.rodionov@gmail.com>
2025-04-24 00:07:32 +0300

    rubocop

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
   def new
     render :new
   end
 
   def create
-    user_info = request.env["omniauth.auth"]
+    user_info = request.env['omniauth.auth']
     raise user_info # Your own session management should be placed here.
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,10 @@
-require_relative "boot"
-require "zip"
-require "base64"
-require "fileutils"
-require "rails/all"
+# frozen_string_literal: true
+
+require_relative 'boot'
+require 'zip'
+require 'base64'
+require 'fileutils'
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
