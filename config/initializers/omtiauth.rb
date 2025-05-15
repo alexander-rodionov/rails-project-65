1780feb8e4f05455acb762296bb7c9492476fa17
alexander-rodionov <alexander.a.rodionov@gmail.com>
2025-05-15 22:00:20 +0300

    test

--- config/initializers/omtiauth.rb
+++ config/initializers/omtiauth.rb
@@ -2,4 +2,5 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'], scope: 'user'
+  pp 'GITHUB_CLIENT_ID', ENV['GITHUB_CLIENT_ID']
 end
