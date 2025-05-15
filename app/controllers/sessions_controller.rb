b8e8043e0309cff93613ecf2bb76b03de0b29543
alexander-rodionov <alexander.a.rodionov@gmail.com>
2025-05-16 00:01:22 +0300

    rubocop

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,13 @@ class SessionsController < ApplicationController
   def create
     auth = request.env['omniauth.auth']
 
-    debugger
     email = auth['info']['email']
     name = auth['info']['name'] || auth['info']['nickname']
     user = User.find_by(email: email)
     user = User.create!(name: name, email: email, admin: true) unless user
     session[:user_id] = user.id
     redirect_to root_path, notice: 'OK'
-  rescue StandardError => e
+  rescue StandardError
     redirect_to root_path, alert: 'ERROR'
   end
 
